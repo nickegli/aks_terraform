e9006cca770c4dd8d1a0b634cdc4f31bd464b128
Yanick Egli <nick.eglirohr@gmail.com>
2023-11-15 10:35:07 +0100

    commented firewall as optional - cost saving

--- .modules/aks/network.tf
+++ .modules/aks/network.tf
@@ -9,38 +9,38 @@ resource "azurerm_resource_group" "net_group" {
 }
 
 
-resource "azurerm_firewall" "firewall" {
-  name                = "Firewall"
-  location            = var.resource_group_location
-  resource_group_name = "${var.service_short_name}-net-${var.environment}-rg"
-  sku_tier = "Standard"
-  sku_name = "AZFW_VNet"
-  ip_configuration {
-    name = "configuration"
-    subnet_id = azurerm_subnet.fw_subnet.id
-    public_ip_address_id = azurerm_public_ip.external_access.id
+# resource "azurerm_firewall" "firewall" {
+#   name                = "Firewall"
+#   location            = var.resource_group_location
+#   resource_group_name = "${var.service_short_name}-net-${var.environment}-rg"
+#   sku_tier = "Standard"
+#   sku_name = "AZFW_VNet"
+#   ip_configuration {
+#     name = "configuration"
+#     subnet_id = azurerm_subnet.fw_subnet.id
+#     public_ip_address_id = azurerm_public_ip.external_access.id
 
-  }
-}
+#   }
+# }
 
-resource "azurerm_firewall_application_rule_collection" "acr_access" {
-  name                = "acrAccessRules"
-  azure_firewall_name = azurerm_firewall.firewall.name
-  resource_group_name = "${var.service_short_name}-net-${var.environment}-rg"
-  priority            = 100
-  action              = "Allow"
+# resource "azurerm_firewall_application_rule_collection" "acr_access" {
+#   name                = "acrAccessRules"
+#   azure_firewall_name = azurerm_firewall.firewall.name
+#   resource_group_name = "${var.service_short_name}-net-${var.environment}-rg"
+#   priority            = 100
+#   action              = "Allow"
 
-  rule {
-    name = "ACRAllow"
-    description = "Allow AKS to ACR"
-    source_addresses = ["10.2.0.0/16"]
-    target_fqdns = ["*.azurecr.io"]
-    protocol {
-      type = "Https"
-      port = 443
-    }
-  }
-}
+#   rule {
+#     name = "ACRAllow"
+#     description = "Allow AKS to ACR"
+#     source_addresses = ["10.2.0.0/16"]
+#     target_fqdns = ["*.azurecr.io"]
+#     protocol {
+#       type = "Https"
+#       port = 443
+#     }
+#   }
+# }
 
 resource "azurerm_public_ip" "external_access" {
   name                = "external_access"
@@ -66,13 +66,13 @@ resource "azurerm_virtual_network" "cluster_vnet" {
 #                  SUBNETS                  #
 #############################################
 
-resource "azurerm_subnet" "fw_subnet" {
-  name                 = "AzureFirewallSubnet"
-  resource_group_name  = "${var.service_short_name}-net-${var.environment}-rg"
-  virtual_network_name = azurerm_virtual_network.cluster_vnet.name
-  address_prefixes     = ["10.0.3.0/24"]
+# resource "azurerm_subnet" "fw_subnet" {
+#   name                 = "AzureFirewallSubnet"
+#   resource_group_name  = "${var.service_short_name}-net-${var.environment}-rg"
+#   virtual_network_name = azurerm_virtual_network.cluster_vnet.name
+#   address_prefixes     = ["10.0.3.0/24"]
 
-}
+# }
 
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "aksSubnet"
